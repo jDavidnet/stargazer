f3809af65ea4d20d154a1f0149ae8211fd84b0ce
Amir Ebrahimi <github@aebrahimi.com>
2014-10-26 23:16:46 -0700

    Initial check-in of LeapMotionRift unity project

---
+++ sg_unity/Assets/Scripts/LeapMotionRift.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using OVR;
+
+public class LeapMotionRift : MonoBehaviour {
+  public Camera normalCamera;
+  public OVRCameraController riftCamera;
+  public HandController leapController;
+
+  private Camera activeCamera;
+
+  public Camera GetActiveCamera()
+  {
+    return activeCamera;
+  }
+
+  private bool isRiftConnected()
+  {
+    Hmd hmd = OVR.Hmd.GetHmd();
+    ovrTrackingState ss = hmd.GetTrackingState();
+    return (ss.StatusFlags & (uint)ovrStatusBits.ovrStatus_HmdConnected) != 0;
+  }
+
+	// Use this for initialization
+	void Start () {
+    Vector3 leapControllerPosition = new Vector3(0.0f, 0.0f, 0.08f); // Leap is positioned outwards by 80cm to mirror the world position
+    if (isRiftConnected())
+    {
+      riftCamera.gameObject.SetActive(true);
+      normalCamera.gameObject.SetActive(false);
+      activeCamera = riftCamera.CameraMain;
+      leapControllerPosition.x = riftCamera.IPD / 2.0f; // Centers the Leap Controller between two Rift cameras
+    }
+    else
+    {
+      normalCamera.gameObject.SetActive(true);
+      riftCamera.gameObject.SetActive(false);
+      activeCamera = normalCamera.camera;
+    }
+    leapController.transform.parent = activeCamera.transform;
+    leapController.transform.rotation = activeCamera.transform.rotation * Quaternion.Euler(270.0f, 180.0f, 0.0f);
+    leapController.transform.localPosition = leapControllerPosition;
+	}
+}
