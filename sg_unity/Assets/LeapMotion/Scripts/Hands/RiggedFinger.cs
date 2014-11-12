4b5032790e70f64d58ecf0c12effde644060c264
Thibaud Lopez Schneider <thibaud_lopez@yahoo.com>
2014-11-12 01:18:11 -0600

    Found simpler code

--- sg_unity/Assets/LeapMotion/Scripts/Hands/RiggedFinger.cs
+++ sg_unity/Assets/LeapMotion/Scripts/Hands/RiggedFinger.cs
@@ -32,11 +32,7 @@ public class RiggedFinger : FingerModel {
     }
     if (this.fingerType == Finger.FingerType.TYPE_INDEX) {
       // Thibaud: raycast index finger tip to infinity; use RiggedPepperCutHand + Gizmos
-      Vector3 fromPosition = GetJointPosition (NUM_BONES - 1);
-      Vector3 toPosition = GetJointPosition (NUM_BONES);
-      Vector3 direction = toPosition - fromPosition;
-      toPosition = fromPosition + direction*100.0f;
-      Debug.DrawLine (fromPosition, toPosition, Color.blue);
+      Debug.DrawRay(GetBoneCenter(3), GetBoneDirection(3) * 100.0f, Color.blue);
     }
   }
 }
