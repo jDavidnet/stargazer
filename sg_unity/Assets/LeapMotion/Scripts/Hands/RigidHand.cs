dacb399d612e8b896bc9ba7ad06b60a67d928b93
Amir Ebrahimi <github@aebrahimi.com>
2014-11-23 19:39:56 -0800

    Checking in WIP for sweeping across sky

--- sg_unity/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
+++ sg_unity/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
@@ -27,9 +27,25 @@ public class RigidHand : SkeletalHand {
   }
 
   public override void UpdateHand() {
+
+	GameObject go = GameObject.FindGameObjectWithTag("CameraCenter");
+
     for (int f = 0; f < fingers.Length; ++f) {
       if (fingers[f] != null)
         fingers[f].UpdateFinger();
+		
+
+//		Vector3 start = fingers[f].GetTipPosition();
+		Vector3 direction = (fingers[f].GetTipPosition() - go.transform.position).normalized; //GetPalmNormal();
+		RaycastHit hit;
+		if (Physics.Raycast(go.transform.position, direction, out hit))
+		{
+//			if (hit.collider.gameObject.name.StartsWith("L_")) {
+//				print ("Finger " + f + " hit: " + hit.collider.gameObject.name);
+//				Debug.DrawRay(go.transform.position, direction);
+				hit.collider.SendMessage("OnMouseOver", SendMessageOptions.DontRequireReceiver);
+//			}
+		}
     }
 
 	Frame frame = controller.Frame();
@@ -59,26 +75,26 @@ public class RigidHand : SkeletalHand {
         palm.rigidbody.angularVelocity = delta_radians * axis / Time.deltaTime;
       }
       // which hand is this?
-      if (hand_.IsLeft) {
-        lineRenderer = FingerLineRenderers.Left;
-      } else if (hand_.IsRight) {
-        lineRenderer = FingerLineRenderers.Right;
-      } else {
-        return;
-      }
-      Vector3 start = GetPalmCenter();
-      Vector3 end = GetPalmNormal() * 100.0f;
-      if (lineRenderer != null) {
-        lineRenderer.SetPosition(0, start);
-        lineRenderer.SetPosition(1, end);
-      }
-      RaycastHit hit;
-      if (Physics.Raycast (start, end, out hit))
-        if (hit.collider)
-          if (hit.collider.gameObject.name.StartsWith("L_")) {
-            print ("Palm hit: " + hit.collider.gameObject.name);
-            hit.collider.SendMessage("OnMouseOver", SendMessageOptions.DontRequireReceiver);
-          }
+//      if (hand_.IsLeft) {
+//        lineRenderer = FingerLineRenderers.Left;
+//      } else if (hand_.IsRight) {
+//        lineRenderer = FingerLineRenderers.Right;
+//      } else {
+//        return;
+//      }
+//      Vector3 start = GetPalmCenter();
+//      Vector3 end = GetPalmNormal() * 100.0f;
+//      if (lineRenderer != null) {
+//        lineRenderer.SetPosition(0, start);
+//        lineRenderer.SetPosition(1, end);
+//      }
+//      RaycastHit hit;
+//      if (Physics.Raycast (start, end, out hit))
+//        if (hit.collider)
+//          if (hit.collider.gameObject.name.StartsWith("L_")) {
+//            print ("Palm hit: " + hit.collider.gameObject.name);
+//            hit.collider.SendMessage("OnMouseOver", SendMessageOptions.DontRequireReceiver);
+//          }
     }
   }
 }
